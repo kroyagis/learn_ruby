df639f58920bd19bba022e08df30bca639b08b35
Ji You <kroyagis@gmail.com>
2016-06-07 15:28:20 -0400

    repeat word method is working now

---
+++ 03_simon_says/simon_says.rb
@@ -0,0 +1,71 @@
+def echo(input)
+  input
+end
+
+def shout(input)
+  input.upcase
+end
+
+def repeat(input, num = 2)
+
+    next_input = " #{input}"
+    input + next_input * (num-1)
+
+
+
+end
+
+# # Wait a second! How can you make the "repeat" method
+# # take one *or* two arguments?
+# #
+# # def repeat(string, num = 2)
+# #   # your code here
+# # end
+# #
+# # The important part is num = 2. What happens is if a num isn't given,
+# # then num will fallback on 2, i.e. it has a default value of 2.
+# def test_repeat_multiple_times
+#   assert_equal "hello hello hello", repeat("hello", 3)
+# end
+#
+# # start_of_word
+# def test_start_of_word_returns_first_letter
+#   assert_equal "h", start_of_word("hello", 1)
+# end
+#
+# def test_start_of_word_returns_first_two_letters
+#   assert_equal "Bo", start_of_word("Bob", 2)
+# end
+#
+# def test_start_of_word_returns_first_many_letters
+#   s = "abcdefg"
+#
+#   assert_equal "a", start_of_word(s, 1)
+#   assert_equal "ab", start_of_word(s, 2)
+#   assert_equal "abc", start_of_word(s, 3)
+# end
+#
+# # first_word
+# def test_first_word
+#   assert_equal "Hello", first_word("Hello World")
+#   assert_equal "oh", first_word("oh dear")
+# end
+#
+# # titleize
+# def test_titleize_capitalizes_a_word
+#   assert_equal "Jaws", titleize("jaws")
+# end
+#
+# # capitalize every word
+# def test_titleize_title_case
+#   assert_equal "David Copperfield", titleize("david copperfield")
+# end
+#
+# def test_titleize_not_little_words
+#   assert_equal "War and Peace", titleize("war and peace")
+# end
+#
+# def test_titleize_little_words_at_start
+#    assert_equal "The Bridge over the River Kwai", titleize("the bridge over the river kwai")
+# end
+# end
