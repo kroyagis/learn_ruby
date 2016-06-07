7ee7fd8cbe7daf470ba01fd58884a1207869cec8
Ji You <kroyagis@gmail.com>
2016-06-07 14:33:07 -0400

    Everything working except extra credits

--- 02_calculator/test_calculator.rb
+++ 02_calculator/test_calculator.rb
@@ -44,25 +44,39 @@ class TestCalculator < MiniTest::Test
   # Extra Credit:
   # If you feel you're up for the challenge, fill in the missing code in the tests below,
   # based off the descriptions, and get them to pass.
-  def test_multiplies_two_numbers
-  end
-
-  def test_multiplies_several_numbers
-  end
-
-# http://en.wikipedia.org/wiki/Factorial
-  def test_computes_factorial_0
-  end
-
-  def test_computes_factorial_1
-  end
-
-  def test_computes_factorial_2
-  end
-
-  def test_computes_factorial_5
-  end
-
-  def test_computes_factorial_10
-  end
+#   def test_multiplies_two_numbers
+#     assert_equal 8, multiply(4, 2)
+#
+#   end
+#
+#   def test_multiplies_several_numbers
+#     assert_equal 25, sum([1,3,5,7,9])
+#
+#   end
+#
+# # http://en.wikipedia.org/wiki/Factorial
+#   def test_computes_factorial_0
+#     assert_equal 25, sum([1,3,5,7,9])
+#
+#   end
+#
+#   def test_computes_factorial_1
+#     assert_equal 25, sum([1,3,5,7,9])
+#
+#   end
+#
+#   def test_computes_factorial_2
+#     assert_equal 25, sum([1,3,5,7,9])
+#
+#   end
+#
+#   def test_computes_factorial_5
+#     assert_equal 25, sum([1,3,5,7,9])
+#
+#   end
+#
+#   def test_computes_factorial_10
+#     assert_equal 25, sum([1,3,5,7,9])
+#
+#   end
 end
