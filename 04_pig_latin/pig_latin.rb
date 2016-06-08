5a295b5a230837a8d9e88ecc863f936df05d2dad
Ji You <kroyagis@gmail.com>
2016-06-07 22:20:08 -0400

    Completed... improve by considering double phoneme case

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,50 +1,45 @@
+# still not perfect..
+# Does not take into consideration the case
+# user input is phoneme followed by phoneme... I think
+# e.g) 'ququiet' 'schschool'
+
+
 def translate(input)
   alphabets = [*'a'..'z']
   vowel = ['a', 'e', 'o', 'i', 'u']
   consonant = (alphabets - vowel)
 
 
-  input_array = input.split(/\s/)
-  master_array = []
+  input_word_array = input.split(/\s/) # splits the user input by white space
+  word_alphabets_array = [] # an array for storing an array of alphabetized user input
 
-  # stores an array of array
-  input_array.each do |x|
-    master_array << x.split(//)
+  master_return = [] # master array which stores the converted piglatins
+
+
+  input_word_array.each do |word|
+    word_alphabets_array << word.split(//)
   end
 
-  master_return = []
-  phoneme_list = ['sch', 'qu']
-  master_array.each do |x|
-    # input starts with vowel
-    if vowel.include? x[0]
-      master_return << (x.join + "ay")
-    end
+  phoneme_list = [['s', 'c', 'h'], ['q', 'u']] # no no list...
 
-    # input starts with consonant
-    return_input = []
-    if consonant.include? x[0]
-      while consonant.include? x[0]
-        return_input << x[0]
-        x.delete_at(0)
+  word_alphabets_array.each do |word_alphabets|
+    consonant_return = [] # resets every loop!!!
+
+    if vowel.include? word_alphabets[0] # when input starts with vowel
+      master_return << (word_alphabets.join + "ay")
+    elsif consonant.include? word_alphabets[0] # when input starts with a consonant
+      phoneme_list.each do |phoneme_word| # check to see if the word contains phoneme
+          while phoneme_word.include? word_alphabets[0]
+            consonant_return << word_alphabets[0]
+            word_alphabets.delete_at(0)
+          end
       end
-      master_return << x.join + return_input.join + "ay"
+      while consonant.include? word_alphabets[0]
+        consonant_return << word_alphabets[0]
+        word_alphabets.delete_at(0)
+      end
+      master_return << word_alphabets.join + consonant_return.join + "ay"
     end
   end
   return master_return.join(' ')
-
 end
-
-# def test_translates_sch_as_phoneme
-#   s = translate("school")
-#   assert_equal "oolschay", s
-# end
-#
-# def test_translates_qu_as_phoneme
-#   s = translate("quiet")
-#   assert_equal "ietquay", s
-# end
-#
-# def test_translates_qu_as_consonant_when_preceded_by_consonant
-#   s = translate("square")
-#   assert_equal "aresquay", s
-# end
